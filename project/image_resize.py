0b122e3a9d119a5d5a0898e744b6fae235582c43
PLT134340 <songwoo1015@gmail.com>
2023-11-07 17:21:49 +0900

    feat : 비율 유지 resize 함수 추가

--- project/image_resize.py
+++ project/image_resize.py
@@ -3,11 +3,24 @@ import numpy as np
 from PIL import Image
 
 
-def resize_dataset_image(path, target_height=200, target_width=200):
+def resize_dataset_image(path, target_height=128, target_width=128):
     image_list = os.listdir(path)
     image_ary = []
     for image in image_list:
         img = Image.open(path + '/' + image).resize((target_height, target_width))
+        img_np = np.array(img)
+        image_ary.append(img_np)
+    return np.array(image_ary)
+
+
+def keep_ratio_resize_dataset_image(path):
+    image_list = os.listdir(path)
+    image_ary = []
+    for image in image_list:
+        img = Image.open(path + '/' + image)
+        width, height = img.size
+        half_min_length = min(width, height) // 2
+        # img.crop((width // 2, height // 2, width // 2, height // 2))
         img_np = np.array(img)
         image_ary.append(img_np)
     return np.array(image_ary)
