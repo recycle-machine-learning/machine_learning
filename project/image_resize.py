2cffd7d1da8b4a7e45c0cc4cea483aa4e2e86027
gjtjdwl <panggoon6@gmail.com>
2023-11-08 13:01:57 +0900

    데이터 y값 붙여서 로드데이터 train_test_split 썼음 사이킷런 패키지 필요

--- project/image_resize.py
+++ project/image_resize.py
@@ -1,8 +1,7 @@
 import os
 import numpy as np
 from PIL import Image
-
-
+from sklearn.model_selection import train_test_split
 def resize_dataset_image(path, target_height=128, target_width=128):
     image_list = os.listdir(path)
     image_ary = []
@@ -11,6 +10,15 @@ def resize_dataset_image(path, target_height=128, target_width=128):
         img_np = np.array(img)
         image_ary.append(img_np)
     return np.array(image_ary)
+
+#train이랑 test 데이터 추출
+def load_data(x,y):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=3,stratify=y)
+    return x_train,x_test,y_train,y_test
+def labeling(path,idx,label):
+    data_num = len(os.listdir(path))
+    for i in range(data_num):
+        label.append(idx)
 
 
 def keep_ratio_resize_dataset_image(path, target_height=128, target_width=128):
