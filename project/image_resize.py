577f8ac50f3a26c12e4c14e465d5180df15283d3
gjtjdwl <panggoon6@gmail.com>
2023-11-08 20:18:16 +0900

    변형시킨 이미지 추가 (인풋 데이터 증가용)

--- project/image_resize.py
+++ project/image_resize.py
@@ -1,25 +1,33 @@
 import os
 import numpy as np
 from PIL import Image
-from sklearn.model_selection import train_test_split
+
 def resize_dataset_image(path, target_height=128, target_width=128):
     image_list = os.listdir(path)
     image_ary = []
+
     for image in image_list:
         img = Image.open(path + '/' + image).convert('RGB').resize((target_height, target_width))
         img_np = np.array(img)
         image_ary.append(img_np)
+        if(len(image_list) < 5000):
+            if(len(image_ary) < 2000):
+                img2 = img.rotate(90) #회전
+                img3 = img.transpose(Image.FLIP_LEFT_RIGHT)  # 좌우반전
+                img4 = img.transpose(Image.FLIP_TOP_BOTTOM)  # 상하반전
+                img5 = img.rotate(180)
+                img6 = img.rotate(250)
+                img2_np =np.array(img2)
+                img3_np =np.array(img3)
+                img4_np =np.array(img4)
+                img5_np =np.array(img5)
+                img6_np =np.array(img6)
+                image_ary.append(img2_np)
+                image_ary.append(img3_np)
+                image_ary.append(img4_np)
+                image_ary.append(img5_np)
+                image_ary.append(img6_np)
     return np.array(image_ary)
-
-#train이랑 test 데이터 추출
-def load_data(x,y):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=3,stratify=y)
-    return x_train,x_test,y_train,y_test
-def labeling(path,idx,label):
-    data_num = len(os.listdir(path))
-    for i in range(data_num):
-        label.append(idx)
-
 
 def keep_ratio_resize_dataset_image(path, target_height=128, target_width=128):
     image_list = os.listdir(path)
@@ -37,3 +45,4 @@ def keep_ratio_resize_dataset_image(path, target_height=128, target_width=128):
         img_np = np.array(resized_img)
         image_ary.append(img_np)
     return np.array(image_ary)
+
