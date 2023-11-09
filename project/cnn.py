64fd1a6b9f8c3feb43121596ada904930a869ee7
tlarbals824 <simkm0824@naver.com>
2023-11-09 11:26:28 +0900

    refactor : out_channels 조정

--- project/cnn.py
+++ project/cnn.py
@@ -10,17 +10,17 @@ class CNN(nn.Module):
         # Image Shape = (?, 128, 128, 3)
         # CONV = (?, 128, 128, 16)
         # POOL = (?, 64, 64, 16)
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1 ,padding=1)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1 ,padding=1)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         # 두 번째 층
         # Image Shape = (?, 64, 64, 16)
         # CONV = (?, 64, 64, 32)
         # POOL = (?, 32, 32, 32)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1,padding=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1,padding=1)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.fc1 = nn.Linear(16 * 16 * 64, 12, bias=True)
+        self.fc1 = nn.Linear(16 * 16 * 32, 12, bias=True)
 
         nn.init.xavier_uniform_(self.fc1.weight)
 
