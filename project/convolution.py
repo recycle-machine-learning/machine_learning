96b1866477c64280e968a7f4ed9a1f98766d9b5f
PLT134340 <songwoo1015@gmail.com>
2023-11-10 17:42:51 +0900

    feat : forward 구현 완료

--- project/convolution.py
+++ project/convolution.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 from torch.nn import Fold, Unfold
 
 
@@ -23,27 +24,46 @@ class Convolution:
         out_w = int(1 + (w + 2 * self.padding - fw) / self.stride)
 
         unfold = Unfold(kernel_size=fw, padding=self.padding, stride=self.stride)
-        col = unfold(x)
-        col_w = self.w.reshape(fn, -1).T
+        col = unfold(x).permute(0, 2, 1)
+        col_w = self.w.view(fn, -1).T
 
         self.x = x
         self.col = col
         self.col_w = col_w
 
-        out = np.dot(col, col_w) + self.b
-        out = out.reshape(n, out_h, out_w, -1).transpose(0, 3, 1, 2)
-        return out
+        y = torch.matmul(col, col_w) + self.b
+        y = y.view(n, out_h, out_w, -1).permute(0, 3, 1, 2)
+        return y
 
     def backward(self, dy):
         fn, c, fh, fw = self.w.shape
-        dy = dy.transpose(0, 2, 3, 1).reshape(-1, fn)
+        dy = dy.permute(0, 2, 3, 1).view(-1, fn)
 
-        self.db = np.sum(dy, axis=0)
-        self.dw = np.dot(self.col.T, dy)
-        self.dw = self.dw.transpose(1, 0).reshape(fn, c, fh, fw)
+        self.db = torch.sum(dy, dim=0)
+        self.dw = torch.matmul(self.col.T, dy)
+        self.dw = self.dw.permute(1, 0).view(fn, c, fh, fw)
 
-        col = np.dot(dy, self.col_w.T)
+        col = torch.matmul(dy, self.col_w.T)
 
         fold = Fold(output_size=2, kernel_size=fw, padding=self.padding, stride=self.stride)
         dx = fold(col)
         return dx
+
+
+if __name__ == '__main__':
+    x_shape = (1, 1, 3, 3)
+    x = torch.randint(0, 5, x_shape).type(torch.FloatTensor)
+    print(x.shape)
+
+    w_shape = (2, 1, 2, 2)
+    w = torch.randint(0, 5, w_shape).type(torch.FloatTensor)
+    print(w.shape)
+
+    b = 1
+    print(b)
+
+    c = Convolution(w, b)
+    c.forward(x)
+
+    # c.backward()
+
