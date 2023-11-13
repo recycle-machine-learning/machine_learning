fe858b7cb1432b92c967b8e612bbdf14c6e20c10
PLT134340 <songwoo1015@gmail.com>
2023-11-13 13:21:28 +0900

    feat : resize 대신 expand 사용

--- project/datatransform/image_resize.py
+++ project/datatransform/image_resize.py
@@ -3,32 +3,39 @@ import os
 import numpy as np
 from PIL import Image
 
+from project.datatransform.resize_image import ResizeImage
+
 
 def resize_dataset_image(path, target_height=128, target_width=128):
     image_list = os.listdir(path)
     image_ary = []
 
     for image in image_list:
-        img = Image.open(path + '/' + image).convert('RGB').resize((target_height, target_width))
-        img_np = np.array(img)
-        image_ary.append(img_np)
-        if len(image_list) < 5000:
-            if len(image_ary) < 2000:
-                img2 = img.rotate(90)   # 회전
-                img3 = img.transpose(Image.FLIP_LEFT_RIGHT)  # 좌우반전
-                img4 = img.transpose(Image.FLIP_TOP_BOTTOM)  # 상하반전
-                img5 = img.rotate(180)
-                img6 = img.rotate(250)
-                img2_np = np.array(img2)
-                img3_np = np.array(img3)
-                img4_np = np.array(img4)
-                img5_np = np.array(img5)
-                img6_np = np.array(img6)
-                image_ary.append(img2_np)
-                image_ary.append(img3_np)
-                image_ary.append(img4_np)
-                image_ary.append(img5_np)
-                image_ary.append(img6_np)
+        # img = Image.open(path + '/' + image).convert('RGB').resize((target_height, target_width))
+        # img_np = np.array(img)
+
+        resize = ResizeImage(size=64, resize_type='expand')
+        img_resized = resize(image)
+        image_ary.append(img_resized)
+
+        # if len(image_list) < 5000:
+        #     if len(image_ary) < 2000:
+        #         img2 = img.rotate(90)   # 회전
+        #         img3 = img.transpose(Image.FLIP_LEFT_RIGHT)  # 좌우반전
+        #         img4 = img.transpose(Image.FLIP_TOP_BOTTOM)  # 상하반전
+        #         img5 = img.rotate(180)
+        #         img6 = img.rotate(250)
+        #         img2_np = np.array(img2)
+        #         img3_np = np.array(img3)
+        #         img4_np = np.array(img4)
+        #         img5_np = np.array(img5)
+        #         img6_np = np.array(img6)
+        #         image_ary.append(img2_np)
+        #         image_ary.append(img3_np)
+        #         image_ary.append(img4_np)
+        #         image_ary.append(img5_np)
+        #         image_ary.append(img6_np)
+
     return np.array(image_ary)
 
 
