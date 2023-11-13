619d35b5d91ab4089c8eb8ba666632c9306e0e2f
tlarbals824 <simkm0824@naver.com>
2023-11-13 17:31:24 +0900

    feat : 직접 구현 convolution 계층 추가

--- project/cnn.py
+++ project/cnn.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 
-from project.layers import relu
+from project.layers import relu, pooling, convolution
 
 
 class CNN(nn.Module):
@@ -11,18 +11,21 @@ class CNN(nn.Module):
         # CONV = (?, 128, 128, 16)
         # POOL = (?, 64, 64, 16)
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.conv1_test = convolution.Convolution(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(16)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.pool1_test = pooling.MaxPooling(kernel_size=2, stride=2, padding=0)
 
         # 두 번째 층
         # Image Shape = (?, 64, 64, 16)
         # CONV = (?, 64, 64, 32)
         # POOL = (?, 32, 32, 32)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.pool2_test = pooling.MaxPooling(kernel_size=2, stride=2, padding=0)
 
-        self.fc1 = nn.Linear(16 * 16 * 64, 12, bias=True)
+        self.fc1 = nn.Linear(16 * 16 * 32, 12, bias=True)
 
         self.drop = nn.Dropout(0.25)
 
@@ -31,14 +34,17 @@ class CNN(nn.Module):
         nn.init.xavier_uniform_(self.fc1.weight)
 
     def forward(self, x):
-        out = self.conv1(x)
+        # out = self.conv1(x)
+        out = self.conv1_test.forward(x)
         out = self.bn1(out)
         out = self.relu.forward(out)
         out = self.pool1(out)
+        # out = self.pool1_test.forward(out)
         out = self.conv2(out)
         out = self.bn2(out)
         out = self.relu.forward(out)
         out = self.pool2(out)
-        out = out.view(out.size(0), -1)
+        # out = self.pool2_test.forward(out)
+        out = out.reshape(out.size(0), -1)
         out = self.fc1(out)
         return out
