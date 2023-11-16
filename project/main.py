7fbe131bdb22fe78ceda3efcf31de2eb9c7a2247
전현민 <77275989+joyjhm@users.noreply.github.com>
2023-11-16 21:49:47 +0900

    feat : grape 추가

--- project/main.py
+++ project/main.py
@@ -3,20 +3,19 @@ import torch
 import torch.nn as nn
 from project.dataloader import image_load
 import matplotlib.pyplot as plt
-
+from auto_encoder import AutoEncoder
 from project.cnn import CNN
 
 if __name__ == '__main__':
     device = torch.device("mps") if torch.backends.mps.is_available() else "cpu"
     # device = "cpu"
 
-
     start = time.time()
 
     model = CNN().to(device)
 
     criterion = nn.CrossEntropyLoss().to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.00005)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
     load_start = time.time()
 
     x_train, y_train, x_test, y_test = image_load.load_data()
@@ -28,19 +27,21 @@ if __name__ == '__main__':
 
     load_end = time.time()
     print('데이터 로드 시간 : {}'.format(load_end - load_start))
-
     total_batch = len(x_train)
+
+
     print('총 배치의 수 : {}'.format(total_batch))
 
     epochs = 10
     batch_size = 50
 
     accuracy_list = []
+    accuracy_train_list = []
 
 
     for epoch in range(epochs):
         avg_cost = 0
-
+        prediction_train = []
         for i in range(0, total_batch, batch_size):
             max_idx = min(i + batch_size, total_batch)
             batch_x = x_train[i:max_idx].to(device)
@@ -57,9 +58,10 @@ if __name__ == '__main__':
         prediction_train = torch.cat(prediction_train, 0)
         correct_prediction_train = torch.argmax(prediction_train, dim=1) == y_train
         train_accuracy = correct_prediction_train.float().mean()
-
+        accuracy_train_list.append(train_accuracy.item())
         print('[Epoch: {:>4}] cost = {:>.9}'.format(epoch + 1, avg_cost))
         print('training Accuracy:', train_accuracy.item())
+
         prediction = model(x_test)
         correct_prediction = torch.argmax(prediction, dim=1) == y_test
         accuracy = correct_prediction.float().mean()
@@ -72,9 +74,11 @@ if __name__ == '__main__':
 
     # label x = range(0, 20)
     # accuracy_list y = accuracy_list
-    plt.plot(accuracy_list)
+    plt.plot(accuracy_list, 'b-', label= 'Test Accuracy')
+    plt.plot(accuracy_train_list, 'r-',label= "Train Accuracy")
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
+    plt.legend()
     plt.show()
 
 
