c208f141cefb990edef19be41a93280e228ee753
전현민 <77275989+joyjhm@users.noreply.github.com>
2023-11-22 21:14:38 +0900

    feat: cnn 생성자 변경, 그래프 하어퍼 파라미터 출력, 하이퍼 파라미터 분리

--- project/cnn.py
+++ project/cnn.py
@@ -4,16 +4,16 @@ from layers import *
 
 
 class CNN(nn.Module):
-    def __init__(self):
+    def __init__(self, size, out_channel1, out_channel2):
         super(CNN, self).__init__()
         # 첫 번째 층
         # Image Shape = (?, 128, 128, 3)
         # CONV = (?, 128, 128, 16)
         # POOL = (?, 64, 64, 16)
         # self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
-        self.conv1_test = Convolution(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.conv1_test = Convolution(in_channels=3, out_channels=out_channel1, kernel_size=3, stride=1, padding=1)
         # self.bn1 = nn.BatchNorm2d(16)
-        self.bn1_test = BatchNormalization(16)
+        self.bn1_test = BatchNormalization(out_channel1)
         # self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.pool1_test = MaxPooling(kernel_size=2, stride=2, padding=0)
 
@@ -22,14 +22,14 @@ class CNN(nn.Module):
         # CONV = (?, 64, 64, 32)
         # POOL = (?, 32, 32, 32)
         # self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.conv2_test = Convolution(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv2_test = Convolution(in_channels=out_channel1, out_channels=out_channel2, kernel_size=3, stride=1, padding=1)
         # self.bn2 = nn.BatchNorm2d(32)
-        self.bn2_test = BatchNormalization(32)
+        self.bn2_test = BatchNormalization(out_channel2)
         # self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.pool2_test = MaxPooling(kernel_size=2, stride=2, padding=0)
 
         # self.fc1 = nn.Linear(8 * 8 * 32, 12, bias=True)
-        self.fc1_test = Affine(8 * 8 * 32, 12, bias=True)
+        self.fc1_test = Affine((size // 4) * (size // 4) * out_channel2, 12, bias=True)
 
         # self.drop1 = nn.Dropout(0.2)
         # self.drop2 = nn.Dropout(0.2)
