300d8e65e909eacbf656b7c3130b873a4c62d516
tlarbals824 <simkm0824@naver.com>
2023-11-22 13:51:49 +0900

    refactor : Batch Norm  수정

--- project/layers/batch_norm.py
+++ project/layers/batch_norm.py
@@ -18,6 +18,7 @@ class BatchNormalization(Module):
         self.epsilon = epsilon  # 0으로 나누는 것을 방지하기 위한 작은 값
         self.momentum = momentum
         self.affine = affine
+        self.input_shape = None
 
         if self.affine:
             self.weight = Parameter(torch.ones(num_features, **factory_kwargs))  # gamma
@@ -32,8 +33,8 @@ class BatchNormalization(Module):
         self.x_norm = None
         self.std = None
 
-        self.d_weight = None
-        self.d_bias = None
+        self.weight.grad = None
+        self.bias.grad = None
 
     def reset_parameters(self) -> None:
         if self.affine:
@@ -42,21 +43,22 @@ class BatchNormalization(Module):
 
     def forward(self, x, is_training=True):
         batch_size, channel, height, width = x.shape
-        x_shape = x.shape
+        self.input_shape = x.shape
 
         self.batch_size = batch_size
-        x = x.reshape(self.batch_size, -1)
 
         if self.running_mean is None:
-            self.running_mean = torch.zeros(x.shape[1])
-            self.running_var = torch.zeros(x.shape[1])
+            self.running_mean = torch.zeros(channel)
+            self.running_var = torch.zeros(channel)
 
         if is_training:
-            mean = torch.mean(x)  # 평균 (1,)
-            xc = x - mean  # 평균을 0으로 (batch_size, channel * height * width)
-            var = torch.mean(xc ** 2)  # 분산 (1,)
-            std = torch.sqrt(var + self.epsilon)  # 표준편차 (batch_size, 1)
-            x_norm = xc / std  # 정규화 (batch_size, channel * height * width)
+            mean = torch.mean(x, dim=(0, 2, 3))
+            mean_view = mean.view(1, -1, 1, 1)  # 평균 (1,)
+            xc = x - mean_view  # 평균을 0으로 (batch_size, channel * height * width)
+            var = torch.mean(xc ** 2, dim=(0, 2, 3))
+            var_view = var.view(1, -1, 1, 1)  # 분산 (channel * height * width,)
+            std = torch.sqrt(var_view + self.epsilon)  # 표준편차 (batch_size, 1)
+            x_norm = xc / std  # 정규화 (batch_size, channel ,height ,width)
 
             self.xc = xc
             self.x_norm = x_norm
@@ -67,28 +69,34 @@ class BatchNormalization(Module):
             xc = x - self.running_mean
             x_norm = xc / (torch.sqrt(self.running_var + self.epsilon))
 
-        x_norm = x_norm.reshape(x_shape)
+        x_norm = x_norm.view(*self.input_shape)
 
-        weight = self.weight.reshape((1, self.num_features, 1, 1))
-        bias = self.bias.reshape((1, self.num_features, 1, 1))
+        weight = self.weight.view((1, -1, 1, 1))
+        bias = self.bias.view((1, -1, 1, 1))
 
         out = weight * x_norm + bias
 
         return out
 
     def backward(self, dout):
-        dout = dout.reshape(self.batch_size, -1)
+        batch_size = dout.shape[0]
 
-        dx_norm = dout * self.weight
+        weight = self.weight.view((1, -1, 1, 1))
+
+        dx_norm = dout * weight
+
         dxc = dx_norm / self.std
-        dstd = -torch.sum((dx_norm * self.xc) / (self.std * self.std), dim=1, keepdim=True)
-        dvar = 0.5 * dstd / self.std
-        dxc += (2.0 / self.batch_size) * self.xc * dvar
-        dmean = torch.sum(dxc, dim=1, keepdim=True)
-        dx = dxc - dmean / self.batch_size
+        dstd = -torch.sum((dx_norm * self.xc) / (self.std * self.std), dim=(0, 2, 3))
+        dstd_view = dstd.view(1, -1, 1, 1)  # (channel * height * width,)
+        dvar = 0.5 * dstd_view / self.std  # (channel * height * width,)
+        dxc += (2.0 / batch_size) * self.xc * dvar  # (batch_size, channel * height * width)
+        dmean = torch.sum(dxc, dim=(0, 2, 3))  # (channel * height * width,)
+        dmean_view = dmean.view(1, -1, 1, 1)
+        dx = dxc - dmean_view / batch_size  # (batch_size, channel * height * width)
 
-        dx = dx.reshape(self.batch_size, *self.x_shape[1:])
-        self.d_weight = torch.sum(dout * self.x_norm, dim=(0, 2, 3))
-        self.d_bias = torch.sum(dout, dim=(0, 2, 3))
+        self.weight.grad = torch.sum(dout * self.x_norm, dim=(0, 2, 3), dtype=torch.float32)
+        self.bias.grad = dout.sum(dim=(0, 2, 3), dtype=torch.float32)
+
+        dx = dx.reshape(*dout.shape)
 
         return dx
