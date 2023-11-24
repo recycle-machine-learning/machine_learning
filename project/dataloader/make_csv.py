3173e6c8885b3ce5ae38a89d8381709e83679efa
PLT134340 <songwoo1015@gmail.com>
2023-11-24 14:06:22 +0900

    feat : 클래스 개수 기반 가중치 적용

--- project/dataloader/make_csv.py
+++ project/dataloader/make_csv.py
@@ -5,11 +5,12 @@ import pandas as pd
 import numpy as np
 
 
-def save_csv(img_dir="dataset/garbage_classification", ratio=0.9) -> None:
+def save_csv(img_dir="dataset/garbage_classification", ratio=0.9) -> np.ndarray:
     """
-    클래스 별로 비율에 따라 train_data, test_data를 임의로 나누고 순서를 섞어 csv 파일로 저장
+    클래스 별로 비율에 따라 train_data, test_data를 임의로 나누고 csv 파일로 저장
     :param img_dir: 이미지 데이터셋 경로
     :param ratio: train_data 비율, 나머지는 test_dada
+    :return: 클래스별 데이터 개수 리스트
     """
     img_dir_list = natsort.natsorted(os.listdir(img_dir))
 
@@ -17,6 +18,8 @@ def save_csv(img_dir="dataset/garbage_classification", ratio=0.9) -> None:
     train_label_list = np.empty(0, dtype=np.int32)
     test_name_list = np.empty(0)
     test_label_list = np.empty(0, dtype=np.int32)
+
+    class_length_list = np.empty(0, dtype=np.int32)
 
     for i, path in enumerate(img_dir_list):
         joined_path = os.path.join(img_dir, path)
@@ -26,6 +29,7 @@ def save_csv(img_dir="dataset/garbage_classification", ratio=0.9) -> None:
 
         idx_permute = np.random.permutation(length)  # 랜덤 인덱스
         train_length = int(length * ratio)
+        class_length_list = np.append(class_length_list, train_length)
 
         train_join_list = [os.path.join(path, img) for img in img_list[idx_permute[:train_length]]]
         train_name_list = np.append(train_name_list, train_join_list)
@@ -36,31 +40,33 @@ def save_csv(img_dir="dataset/garbage_classification", ratio=0.9) -> None:
         test_label_list = np.append(test_label_list, [i] * (length - train_length))
 
     # 클래스 별로 나누어진 리스트를 재배열
-    train_idx_permute = np.random.permutation(len(train_name_list))
-    test_idx_permute = np.random.permutation(len(test_name_list))
+    # train_idx_permute = np.random.permutation(len(train_name_list))
+    # train_name_list = train_name_list[train_idx_permute]
+    # train_label_list = train_label_list[train_idx_permute]
+    #
+    # test_idx_permute = np.random.permutation(len(test_name_list))
+    # test_name_list = test_name_list[test_idx_permute]
+    # test_label_list = test_label_list[test_idx_permute]
 
-    train_name_list = train_name_list[train_idx_permute]
-    train_label_list = train_label_list[train_idx_permute]
-    test_name_list = test_name_list[test_idx_permute]
-    test_label_list = test_label_list[test_idx_permute]
+    train = {"file_name": train_name_list,
+             "label": train_label_list}
 
-    train = {'file_name': train_name_list,
-             'label': train_label_list}
-
-    test = {'file_name': test_name_list,
-            'label': test_label_list}
+    test = {"file_name": test_name_list,
+            "label": test_label_list}
 
     df1 = pd.DataFrame(train)
-    df1.to_csv('train_data.csv', index=False)
+    df1.to_csv("train_data.csv", index=False)
 
     df2 = pd.DataFrame(test)
-    df2.to_csv('test_data.csv', index=False)
+    df2.to_csv("test_data.csv", index=False)
+
+    return class_length_list
 
 
-def load_csv(csv_path='annotations.csv'):
+def load_csv(csv_path="annotations.csv"):
     csv = pd.read_csv(csv_path)
     print(csv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     save_csv()
